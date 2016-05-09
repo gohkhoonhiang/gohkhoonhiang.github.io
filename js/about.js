ee9206a4e202aaf3c545fd71ef22662344488a13
Goh Khoon Hiang <gohkhoonhiang@gmail.com>
2016-05-09 22:46:51 +0800

    Use warning class for form validation

--- js/about.js
+++ js/about.js
@@ -17,38 +17,38 @@ $(document).ready(function() {
                 if (!contactName) {
                     $('#contact-name-error').html("I would love to know who you are. Perhaps you could leave your name?");
                     $('#contact-name-error').show();
-                    $('#contact-name').parent().addClass("has-error");
+                    $('#contact-name').parent().addClass("has-warning");
                     return;
                 } else {
                     $('#contact-name-error').html("");
                     $('#contact-name-error').hide();
-                    $('#contact-name').parent().removeClass("has-error");
+                    $('#contact-name').parent().removeClass("has-warning");
                 }
                 if (!contactEmail && !contactNumber) {
                     $('#contact-email-error').html("I would love to know how to get back to you. Your email will be much appreciated.");
                     $('#contact-email-error').show();
-                    $('#contact-email').parent().addClass("has-error");
+                    $('#contact-email').parent().addClass("has-warning");
                     $('#contact-number-error').html("Or if you could allow me to contact you through the phone.");
                     $('#contact-number-error').show();
-                    $('#contact-number').parent().addClass("has-error");
+                    $('#contact-number').parent().addClass("has-warning");
                     return;
                 } else {
                     $('#contact-email-error').html("");
                     $('#contact-email-error').hide();
-                    $('#contact-email').parent().removeClass("has-error");
+                    $('#contact-email').parent().removeClass("has-warning");
                     $('#contact-number-error').html("");
                     $('#contact-number-error').hide();
-                    $('#contact-number').parent().removeClass("has-error");
+                    $('#contact-number').parent().removeClass("has-warning");
                 }
                 if (!contactPurpose) {
                     $('#contact-purpose-error').html("I'm eager to learn first-hand what you might want to share!");
                     $('#contact-purpose-error').show();
-                    $('#contact-purpose').parent().addClass("has-error");
+                    $('#contact-purpose').parent().addClass("has-warning");
                     return;
                 } else {
                     $('#contact-purpose-error').html("");
                     $('#contact-purpose-error').hide();
-                    $('#contact-purpose').parent().removeClass("has-error");
+                    $('#contact-purpose').parent().removeClass("has-warning");
                 }
 		var url = "//docs.google.com/forms/d/16S1tuPQY_g0atfjLNnGxv5vyo3BiWThXoYchwOArXy0/formResponse";
 		var data = {
@@ -79,15 +79,15 @@ $(document).ready(function() {
                 console.log($(this));
                 $('#contact-email').next('.help-block').html("");
                 $('#contact-email').next('.help-block').hide();
-                $('#contact-email').parent().removeClass("has-error");
+                $('#contact-email').parent().removeClass("has-warning");
                 $('#contact-number').next('.help-block').html("");
                 $('#contact-number').next('.help-block').hide();
-                $('#contact-number').parent().removeClass("has-error");
+                $('#contact-number').parent().removeClass("has-warning");
             }
             else if ($(this).val()) {
                 $(this).next('.help-block').html("");
                 $(this).next('.help-block').hide();
-                $(this).parent().removeClass("has-error");
+                $(this).parent().removeClass("has-warning");
             }
         });
 });
